d0a49544c9298aef3f05be0d8232797e73d3bb69
Yiding He <yiding.he@gmail.com>
2016-12-22 18:13:34 +0800

    初始化

--- xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/controllers/GenerateDataController.java
+++ xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/controllers/GenerateDataController.java
@@ -2,12 +2,16 @@ package com.xz.scorep.executor.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class GenerateDataController {
 
     @PostMapping("/generate_data")
-    public String generateData() {
+    @ResponseBody
+    public String generateData(HttpServletRequest request) {
         return "数据已经生成";
     }
 }
