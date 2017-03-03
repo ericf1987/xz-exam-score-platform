438521a9988608a23406be1a36f8614b8255fcc5
yiding he <yiding.he@gmail.com>
2017-03-03 22:37:17 +0800

    分数段统计

---
+++ xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/exportexcel/impl/total/TotalSchoolSegmentSheet0.java
@@ -0,0 +1,20 @@
+package com.xz.scorep.executor.exportexcel.impl.total;
+
+import com.xz.scorep.executor.exportexcel.SheetContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TotalSchoolSegmentSheet0 extends TotalSchoolSegmentSheet {
+
+    protected String getTargetType(SheetContext sheetContext) {
+        return "project";
+    }
+
+    protected String getSubjectName(SheetContext sheetContext) {
+        return "总分";
+    }
+
+    protected String getTargetIdCondition(SheetContext sheetContext) {
+        return "";
+    }
+}
