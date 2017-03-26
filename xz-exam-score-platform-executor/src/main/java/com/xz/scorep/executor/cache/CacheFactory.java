5612c681ff0931a6ddc71b7ea1dd846d761186d0
Yiding He <yiding.he@gmail.com>
2017-03-26 16:19:13 +0800

    当查询尚未导入的项目的时候应该报错

--- xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/cache/CacheFactory.java
+++ xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/cache/CacheFactory.java
@@ -4,13 +4,26 @@ import com.hyd.simplecache.EhCacheConfiguration;
 import com.hyd.simplecache.SimpleCache;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class CacheFactory {
 
-    private Map<String, SimpleCache> cacheMap = new HashMap<>();
+    private final Map<String, SimpleCache> cacheMap = new HashMap<>();
+
+    private SimpleCache globalCache;
+
+    @PostConstruct
+    private void initCacheFactory() {
+        globalCache = createCache("global");
+    }
+
+    public SimpleCache getGlobalCache() {
+        return globalCache;
+    }
 
     public SimpleCache getProjectCache(String projectId) {
         SimpleCache result = cacheMap.get(projectId);
@@ -24,11 +37,21 @@ public class CacheFactory {
     }
 
     private SimpleCache createProjectCache(String projectId) {
+        return createCache("project:" + projectId);
+    }
+
+    private SimpleCache createCache(String cacheName) {
         EhCacheConfiguration conf = new EhCacheConfiguration();
-        conf.setName("project:" + projectId);
+        conf.setName(cacheName);
         conf.setMaxEntriesLocalHeap(100000);
         conf.setTimeToLiveSeconds(3600);
         conf.setTimeToIdleSeconds(3600);
         return new SimpleCache(conf);
+    }
+
+    @PreDestroy
+    private void close() {
+        this.globalCache.close();
+        this.cacheMap.values().forEach(SimpleCache::close);
     }
 }
--- xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/db/DAOFactory.java
+++ xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/db/DAOFactory.java
@@ -3,7 +3,10 @@ package com.xz.scorep.executor.db;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.hyd.dao.DAO;
 import com.hyd.dao.DataSources;
+import com.hyd.dao.Row;
+import com.hyd.simplecache.SimpleCache;
 import com.xz.ajiaedu.common.lang.StringUtil;
+import com.xz.scorep.executor.cache.CacheFactory;
 import com.xz.scorep.executor.config.DbConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -23,6 +26,9 @@ public class DAOFactory {
 
     @Autowired
     private DbConfig dbConfig;
+
+    @Autowired
+    private CacheFactory cacheFactory;
 
     private DataSources dataSources = new DataSources();
 
@@ -49,7 +55,20 @@ public class DAOFactory {
 
     // 获得项目库连接 DAO
     public synchronized DAO getProjectDao(String projectId) {
+
+        SimpleCache globalCache = cacheFactory.getGlobalCache();
+        String missingCacheKey = "missing:" + projectId;
+        if (globalCache.get(missingCacheKey) != null) {
+            throw new IllegalStateException("项目 " + projectId + " 尚未导入。");
+        }
+
         if (!dataSources.contains(projectId)) {
+
+            Row row = getManagerDao().queryFirst("select 1 from project where id=?", projectId);
+            if (row == null) {
+                globalCache.put(missingCacheKey, true, 60);
+            }
+
             dataSources.setDataSource(projectId, getProjectDataSource(projectId));
         }
         return dataSources.getDAO(projectId);
