60b8b1aa411b85cdf55f1811f00e5042efd25aba
luckylo <zhg578@163.com>
2017-03-26 13:26:44 +0800

    修复Bug

--- xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/exportexcel/ExportExcelController.java
+++ xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/exportexcel/ExportExcelController.java
@@ -2,12 +2,15 @@ package com.xz.scorep.executor.exportexcel;
 
 import com.xz.ajiaedu.common.lang.Result;
 import com.xz.scorep.executor.utils.AsyncCounter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ExportExcelController {
+    private final Logger LOG = LoggerFactory.getLogger(ExportExcelController.class);
 
     @Autowired
     private ExcelReportManager excelReportManager;
@@ -17,7 +20,8 @@ public class ExportExcelController {
     public Result exportExcel(@RequestParam("projectId") String projectId) {
 
         if (this.excelReportManager.isRunning(projectId)) {
-            return Result.fail("正在执行 Excel 生成。");
+            LOG.info("项目ID{}Excel报表正在生成,请勿重复导出Excel报表...", projectId);
+            return Result.fail(1,"正在执行 Excel 生成,请耐心等待...。");
         }
 
         this.excelReportManager.generateReports(projectId, false);
