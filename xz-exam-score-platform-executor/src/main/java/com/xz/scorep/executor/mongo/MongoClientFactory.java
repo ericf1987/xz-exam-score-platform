f073c8b3b86a6c8f4e4ce18e9d9d883e7c1248c9
luckylo <zhg578@163.com>
2017-04-13 09:40:52 +0800

    测试代码,日志

--- xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/mongo/MongoClientFactory.java
+++ xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/mongo/MongoClientFactory.java
@@ -3,17 +3,23 @@ package com.xz.scorep.executor.mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 import com.xz.scorep.executor.config.MongoConfig;
+import com.xz.scorep.executor.importproject.ImportProjectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.xz.ajiaedu.common.mongo.MongoUtils.doc;
 
 @Service
 public class MongoClientFactory {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MongoClientFactory.class);
 
     @Autowired
     private MongoConfig mongoConfig;
@@ -41,14 +47,18 @@ public class MongoClientFactory {
      * 根据项目 ID 查询其对应的 MongoClient
      *
      * @param projectId 项目ID
-     *
      * @return 包含该项目的 MongoClient
      */
     public MongoClient getProjectMongoClient(String projectId) {
-        return this.scannerMongoClients.stream()
+
+        MongoClient mongoClient = this.scannerMongoClients.stream()
                 .filter(client -> projectExists(client, projectId))
                 .findFirst()
                 .orElse(null);
+
+        LOG.info("项目 {} 所属网阅数据库：{}", projectId, mongoClient.getAddress());
+
+        return mongoClient;
     }
 
     private boolean projectExists(MongoClient client, String project) {
@@ -56,6 +66,13 @@ public class MongoClientFactory {
                 .getDatabase("project_database")
                 .getCollection("project")
                 .count(doc("projectId", project)) > 0;
+    }
+
+
+    public List<MongoClient> getScannerMongoClients(String projectId) {
+        return this.scannerMongoClients.stream()
+                .filter(client -> projectExists(client, projectId))
+                .collect(Collectors.toList());
     }
 
 }
