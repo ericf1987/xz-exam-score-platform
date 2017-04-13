91c91d644ba218139cfb2ac3bc4fd5856a7aeb32
Yiding He <yiding.he@gmail.com>
2017-04-13 10:45:50 +0800

    修改 absent/lost/cheat 表结构

--- xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/project/CheatService.java
+++ xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/project/CheatService.java
@@ -1,12 +1,9 @@
 package com.xz.scorep.executor.project;
 
 import com.hyd.dao.DAO;
-import com.xz.ajiaedu.common.lang.MapBuilder;
 import com.xz.scorep.executor.db.DAOFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 @Service
 public class CheatService {
@@ -17,13 +14,8 @@ public class CheatService {
     public void saveCheat(String projectId, String studentId, String subjectId) {
         DAO projectDao = daoFactory.getProjectDao(projectId);
 
-        String countSql = "select count(1) from cheat where student_id=? and subject_id=?";
-        int count = projectDao.count(countSql, studentId, subjectId);
-
-        if (count == 0) {
-            Map<String, String> map = MapBuilder.start("student_id", studentId).and("subject_id", subjectId).get();
-            projectDao.insert(map, "cheat");
-        }
+        projectDao.execute("insert ignore into cheat set student_id=?,subject_id=?",
+                studentId, subjectId);
     }
 
     public void clearCheat(String projectId){
--- xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/project/LostService.java
+++ xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/project/LostService.java
@@ -1,12 +1,9 @@
 package com.xz.scorep.executor.project;
 
 import com.hyd.dao.DAO;
-import com.xz.ajiaedu.common.lang.MapBuilder;
 import com.xz.scorep.executor.db.DAOFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 /**
  * 缺卷处理
@@ -20,13 +17,8 @@ public class LostService {
     public void saveLost(String projectId, String studentId, String subjectId) {
         DAO projectDao = daoFactory.getProjectDao(projectId);
 
-        String countSql = "select count(1) from lost where student_id=? and subject_id=?";
-        int count = projectDao.count(countSql, studentId, subjectId);
-
-        if (count == 0) {
-            Map<String, String> map = MapBuilder.start("student_id", studentId).and("subject_id", subjectId).get();
-            projectDao.insert(map, "lost");
-        }
+        projectDao.execute("insert ignore into lost set student_id=?,subject_id=?",
+                studentId, subjectId);
     }
 
     public void clearLost(String projectId) {
