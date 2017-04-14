6baadff36735282712ab69873daadde3ff49b82e
luckylo <zhg578@163.com>
2017-04-14 16:51:23 +0800

    待监控平台提供测试项目测试

--- xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/importproject/ImportStudentHelper.java
+++ xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/importproject/ImportStudentHelper.java
@@ -2,6 +2,8 @@ package com.xz.scorep.executor.importproject;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import com.xz.ajiaedu.common.ajia.Param;
 import com.xz.ajiaedu.common.appauth.AppAuthClient;
 import com.xz.ajiaedu.common.json.JSONUtils;
@@ -14,6 +16,7 @@ import com.xz.scorep.executor.bean.ProjectStudent;
 import com.xz.scorep.executor.project.ClassService;
 import com.xz.scorep.executor.project.SchoolService;
 import com.xz.scorep.executor.project.StudentService;
+import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,6 +24,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
+
+import static com.xz.ajiaedu.common.mongo.MongoUtils.doc;
 
 /**
  * (description)
@@ -54,16 +60,78 @@ public class ImportStudentHelper {
     }
 
     public void importStudentList(Context context) {
+        String projectId = context.get("projectId");
+        MongoClient client = context.get("client");
 
-        importSchools(context);
-        importClasses(context);
-        importStudents(context);
+        clearBaseInfo(projectId);
+
+        MongoDatabase projectDb = client.getDatabase(projectId);
+        Map<String, ProjectSchool> schools = new HashMap<>();
+
+        projectDb.getCollection("schools")
+                .find(doc())
+                .forEach((Consumer<Document>) doc -> importSchools(projectId, schools, doc));
+
+        projectDb.getCollection("studentForProject")
+                .find(doc())
+                .forEach((Consumer<Document>) doc -> importStudentAndClass(projectId, schools, doc));
+
+
+//        importSchools(context);
+//        importClasses(context);
+//        importStudents(context);
 
         this.studentCounter.finish();
     }
 
-    private void importSchools(Context context) {
+    private void importStudentAndClass(String projectId, Map<String, ProjectSchool> schools, Document document) {
 
+        String classId = document.getString("classId");
+        String className = document.getString("className");
+        String schoolId = document.getString("schoolId");
+
+        ProjectSchool school = schools.get(schoolId);
+        String area = school.getArea();
+        String city = school.getCity();
+        String province = school.getProvince();
+
+        ProjectClass projectClass = new ProjectClass(classId, className, schoolId, area, city, province);
+        classService.saveClass(projectId, projectClass);
+
+        String studentId = document.getString("studentId");
+        String studentName = document.getString("studentName");
+        String examNo = document.getString("examNo");
+        String schoolStudentNo = document.getString("schoolStudentNo");
+        ProjectStudent student = new ProjectStudent(studentId, examNo,
+                schoolStudentNo, studentName, classId, schoolId, area, city, province);
+        studentService.saveStudent(projectId, student);
+
+
+    }
+
+    private void importSchools(String projectId, Map<String, ProjectSchool> schools, Document document) {
+        String schoolId = document.getString("schoolId");
+        String schoolName = document.getString("schoolName");
+        String area = document.getString("areaId");
+        String city = document.getString("cityId");
+        String province = document.getString("provinceId");
+
+        ProjectSchool school = new ProjectSchool(schoolId, schoolName, area, city, province);
+        schools.put(schoolId, school);
+        schoolService.saveSchool(projectId, school);
+
+    }
+
+    private void clearBaseInfo(String projectId) {
+        // 清空学校列表
+        schoolService.clearSchools(projectId);
+        // 清空班级列表
+        classService.clearClasses(projectId);
+        // 清空考生列表
+        studentService.clearStudents(projectId);
+    }
+
+    private void importSchools(Context context) {
         Map<String, ProjectSchool> contextSchools = new HashMap<>();
         context.put("schools", contextSchools);
 
