6797822553808a5b7e1a0b3a72e9aeaca011f36a
luckylo <zhg578@163.com>
2017-04-17 15:07:03 +0800

    导入学生信息删除无用方法

--- xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/importproject/ImportStudentHelper.java
+++ xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/importproject/ImportStudentHelper.java
@@ -101,7 +101,7 @@ public class ImportStudentHelper {
                 schoolStudentNo, studentName, classId, schoolId, area, city, province);
         studentService.saveStudent(projectId, student);
 
-
+        studentCounter.incre();
     }
 
     private void importSchools(String projectId, Map<String, ProjectSchool> schools, Document document) {
@@ -124,94 +124,6 @@ public class ImportStudentHelper {
         classService.clearClasses(projectId);
         // 清空考生列表
         studentService.clearStudents(projectId);
-    }
-
-    private void importSchools(Context context) {
-        Map<String, ProjectSchool> contextSchools = new HashMap<>();
-        context.put("schools", contextSchools);
-
-        String projectId = context.get("projectId");
-        Result result = appAuthClient.callApi("QueryExamSchoolByProject",
-                new Param().setParameter("projectId", projectId)
-                        .setParameter("needStudentCount", false));
-
-        // 清空学校列表
-        schoolService.clearSchools(projectId);
-
-        // 重新保存学校列表
-        JSONArray schools = result.get("schools");
-        for (int i = 0; i < schools.size(); i++) {
-            ProjectSchool school = schools.getObject(i, ProjectSchool.class);
-            contextSchools.put(school.getId(), school);
-            schoolService.saveSchool(projectId, school);
-        }
-    }
-
-    private void importClasses(Context context) {
-
-        Map<String, ProjectClass> contextClasses = new HashMap<>();
-        context.put("classes", contextClasses);
-
-        String projectId = context.get("projectId");
-        Map<String, ProjectSchool> contextSchools = context.get("schools");
-
-        // 清空班级列表
-        classService.clearClasses(projectId);
-
-        // 重新保存班级列表
-        List<ProjectClass> classList = new ArrayList<>();
-
-        for (ProjectSchool school : contextSchools.values()) {
-            Result result = appAuthClient.callApi("QueryExamClassByProject",
-                    new Param().setParameter("projectId", projectId)
-                            .setParameter("schoolId", school.getId())
-                            .setParameter("needStudentCount", false));
-
-            JSONArray classes = result.get("classes");
-            JSONUtils.<JSONObject>forEach(classes, o -> {
-                ProjectClass c = new ProjectClass(o);
-                c.setSchoolId(school.getId());
-                c.setArea(school.getArea());
-                c.setCity(school.getCity());
-                c.setProvince(school.getProvince());
-
-                contextClasses.put(c.getId(), c);
-                classList.add(c);
-            });
-        }
-
-        classService.saveClass(projectId, classList);
-    }
-
-    private void importStudents(Context context) {
-        String projectId = context.getString("projectId");
-        Map<String, ProjectClass> contextClasses = context.get("classes");
-
-        // 清空考生列表
-        studentService.clearStudents(projectId);
-
-        for (ProjectClass projectClass : contextClasses.values()) {
-            Result result = appAuthClient.callApi("QueryClassExamStudent",
-                    new Param().setParameter("projectId", projectId)
-                            .setParameter("classId", projectClass.getId()));
-
-            JSONArray examStudents = result.get("examStudents");
-            List<ProjectStudent> studentList = new ArrayList<>();
-
-            JSONUtils.<JSONObject>forEach(examStudents, s -> {
-                ProjectStudent student = new ProjectStudent(s);
-                student.setClassId(projectClass.getId());
-                student.setSchoolId(projectClass.getSchoolId());
-                student.setArea(projectClass.getArea());
-                student.setCity(projectClass.getCity());
-                student.setProvince(projectClass.getProvince());
-
-                studentList.add(student);
-                studentCounter.incre();
-            });
-
-            studentService.saveStudent(projectId, studentList);
-        }
     }
 
 }
