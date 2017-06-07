024fccee454fd30ad5c20f94e5fb5cf6c8e2c237
wanna <zhg578@163.com>
2017-06-07 14:37:09 +0800

    客观题区分度统计,待优化下....

--- xz-exam-score-platform-executor/src/test/java/com/xz/scorep/executor/aggregate/impl/DistinctionValueAggregatorTest.java
+++ xz-exam-score-platform-executor/src/test/java/com/xz/scorep/executor/aggregate/impl/DistinctionValueAggregatorTest.java
@@ -5,12 +5,6 @@ import com.xz.scorep.executor.aggregate.AggregateParameter;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.Assert.*;
-
 /**
  * @author luckylo
  * @createTime 2017-06-06.
@@ -23,20 +17,6 @@ public class DistinctionValueAggregatorTest extends BaseTest {
     @Test
     public void aggregate() throws Exception {
         aggregator.aggregate(new AggregateParameter("430300-29c4d40d93bf41a5a82baffe7e714dd9"));
-    }
-
-
-    @Test
-    public void test(){
-        List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,10));
-
-        double val = list.stream()
-                .sorted((a,b)->b-a)
-                .limit(2)
-                .mapToDouble(i1 -> i1)
-                .average().getAsDouble();
-        System.out.println(val);
-
     }
 
 }
