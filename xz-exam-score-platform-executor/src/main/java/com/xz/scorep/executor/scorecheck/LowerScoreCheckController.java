f6db787450204aa3f027fd2c1b3ee026b3b06900
wanna <zhg578@163.com>
2017-06-13 18:30:02 +0800

    添加总分,科目,科目主客观题低分检查........

--- xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/scorecheck/LowerScoreCheckController.java
+++ xz-exam-score-platform-executor/src/main/java/com/xz/scorep/executor/scorecheck/LowerScoreCheckController.java
@@ -1,12 +1,17 @@
 package com.xz.scorep.executor.scorecheck;
 
+import com.hyd.dao.Row;
 import com.xz.ajiaedu.common.lang.Result;
+import com.xz.ajiaedu.common.lang.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * 监控平台低分检查
@@ -18,6 +23,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LowerScoreCheckController {
 
     private static final Logger LOG = LoggerFactory.getLogger(LowerScoreCheckController.class);
+
+    @Autowired
+    private LowerScoreService scoreService;
 
     @ResponseBody
     @GetMapping("check/{projectId}/{subjectId}/{score}")
@@ -36,6 +44,14 @@ public class LowerScoreCheckController {
     }
 
     private Result check(String projectId, String subjectId, double score) {
-        return Result.success();
+
+        if (StringUtil.isEmpty(subjectId)) {
+            List<Row> rows = scoreService.queryProjectLowerScoreStudent(projectId, score);
+            return Result.success().set("students", rows);
+        } else {
+            List<Row> rows = scoreService.querySubjectLowerScoreStudent(projectId, subjectId, score);
+            return Result.success().set("students", rows);
+        }
+
     }
 }
